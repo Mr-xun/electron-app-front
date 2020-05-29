fdd9ca76f1af0b916dbafc5373b13b49a560400d
荀潇 <xiao.xun@moji.com>
2020-05-29 19:46:20 +0800

    update 配置上传图片地址

--- src/views/user/components/UserEdit.vue
+++ src/views/user/components/UserEdit.vue
@@ -1,182 +1,170 @@
 <template>
-  <el-dialog
-    :title="editUserTitle"
-    :visible="editUserVisible"
-    class="edituser-dialog"
-    :before-close="closeEditUser"
-    :close-on-click-modal="false"
-  >
-    <el-form :model="editUserForm" label-width="80px" :rules="editUserRules" ref="editUserForm">
-      <el-form-item label="用户名" prop="username">
-        <el-input v-model="editUserForm.username"></el-input>
-      </el-form-item>
-      <el-form-item label="密码" prop="password">
-        <el-input v-model="editUserForm.password" :disabled="editUserId?true:false"></el-input>
-      </el-form-item>
-      <el-form-item label="用户图标">
-        <el-upload
-          class="avatar-uploader"
-          :headers="{authorization: `Bearer ${$store.state.user.token}`}"
-          :action="`http://192.168.0.127:3001/file/upload`"
-          :show-file-list="false"
-          accept="image/png, image/gif, image/jpg, image/jpeg"
-          :on-success="handleAvatarSuccess"
-        >
-          <el-image
-            v-if="editUserForm.avatar"
-            class="avatar"
-            style="width: 100px; height: 100px"
-            :src="editUserForm.avatar"
-            fit="cover"
-          ></el-image>
-          <i v-else class="el-icon-upload avatar-uploader-icon"></i>
-          <div v-if="!editUserForm.avatar" class="avatar-uploader-text">上传照片</div>
-        </el-upload>
-      </el-form-item>
-      <el-form-item label="联系方式">
-        <el-input v-model="editUserForm.phone"></el-input>
-      </el-form-item>
-      <el-form-item label="用户角色" prop="role">
-        <el-select v-model="editUserForm.role" placeholder="请选择用户角色">
-          <el-option label="管理员" value="1"></el-option>
-        </el-select>
-      </el-form-item>
-    </el-form>
-    <div slot="footer" class="dialog-footer">
-      <el-button @click="closeEditUser">取 消</el-button>
-      <el-button type="primary" @click="submitUser()">确 定</el-button>
-    </div>
-  </el-dialog>
+    <el-dialog :title="editUserTitle" :visible="editUserVisible" class="edituser-dialog" :before-close="closeEditUser"
+        :close-on-click-modal="false">
+        <el-form :model="editUserForm" label-width="80px" :rules="editUserRules" ref="editUserForm">
+            <el-form-item label="用户名" prop="username">
+                <el-input v-model="editUserForm.username"></el-input>
+            </el-form-item>
+            <el-form-item label="密码" prop="password">
+                <el-input v-model="editUserForm.password" :disabled="editUserId?true:false"></el-input>
+            </el-form-item>
+            <el-form-item label="用户图标">
+                <el-upload class="avatar-uploader" :headers="{authorization: `Bearer ${$store.state.user.token}`}"
+                    :action="`${baseUrl}/file/upload`" :show-file-list="false"
+                    accept="image/png, image/gif, image/jpg, image/jpeg" :on-success="handleAvatarSuccess">
+                    <el-image v-if="editUserForm.avatar" class="avatar" style="width: 100px; height: 100px"
+                        :src="editUserForm.avatar" fit="cover"></el-image>
+                    <i v-else class="el-icon-upload avatar-uploader-icon"></i>
+                    <div v-if="!editUserForm.avatar" class="avatar-uploader-text">上传照片</div>
+                </el-upload>
+            </el-form-item>
+            <el-form-item label="联系方式">
+                <el-input v-model="editUserForm.phone"></el-input>
+            </el-form-item>
+            <el-form-item label="用户角色" prop="role">
+                <el-select v-model="editUserForm.role" placeholder="请选择用户角色">
+                    <el-option label="管理员" value="1"></el-option>
+                </el-select>
+            </el-form-item>
+        </el-form>
+        <div slot="footer" class="dialog-footer">
+            <el-button @click="closeEditUser">取 消</el-button>
+            <el-button type="primary" @click="submitUser()">确 定</el-button>
+        </div>
+    </el-dialog>
 </template>
 <script lang='ts'>
 import { Component, Prop, Vue, Watch, Emit } from "vue-property-decorator";
 import { UserModule } from "@/store/modules/user";
 import api from "@/api";
 interface IeditUserForm {
-  user_id?: string;
-  username: string;
-  password: string;
-  avatar?: string;
-  phone?: string;
-  role?: string;
+    user_id?: string;
+    username: string;
+    password: string;
+    avatar?: string;
+    phone?: string;
+    role?: string;
 }
 let devBaseUrl = "http://192.168.13.241:3001";
 let proBaseUrl = "http://49.233.16.84:3001";
 let homeBaseUrl = "http://192.168.0.127:3001";
-let baseUrl = process.env.NODE_ENV == "product" ? proBaseUrl : devBaseUrl;
 @Component({
-  name: "UserEdit"
+    name: "UserEdit"
 })
 export default class UserEdit extends Vue {
-  @Prop() editUserForm!: IeditUserForm;
-  @Prop({ default: false, type: Boolean }) editUserVisible!: boolean;
-  @Prop({ default: "注册用户", type: String }) editUserTitle!: string;
-  @Prop({ default: null, type: String }) editUserId!: any;
-  private switch: boolean = false;
-  private editUserRules: object = {
-    username: [{ required: true, message: "请输入用户名", trigger: "blur" }],
-    password: [{ required: true, message: "请输入密码", trigger: "blur" }],
-    role: [{ required: true, message: "请选择用户角色", trigger: "change" }]
-  };
-  @Watch("switch")
-  private changeSwitch() {
-    this.changeVisiable();
-  }
-  @Emit("update:editUserVisible")
-  private changeVisiable() {
-    return false;
-  }
-  @Emit("initList")
-  private sendEmit() {
-    return;
-  }
-  handleAvatarSuccess(res: any, file: any) {
-    if (res.code == 0) {
-      this.editUserForm.avatar = res.result.data;
-    } else {
-      this.editUserForm.avatar = "";
+    @Prop() editUserForm!: IeditUserForm;
+    @Prop({ default: false, type: Boolean }) editUserVisible!: boolean;
+    @Prop({ default: "注册用户", type: String }) editUserTitle!: string;
+    @Prop({ default: null, type: String }) editUserId!: any;
+    private switch: boolean = false;
+    private baseUrl =
+        process.env.NODE_ENV == "production" ? proBaseUrl : devBaseUrl;
+    private editUserRules: object = {
+        username: [
+            { required: true, message: "请输入用户名", trigger: "blur" }
+        ],
+        password: [{ required: true, message: "请输入密码", trigger: "blur" }],
+        role: [{ required: true, message: "请选择用户角色", trigger: "change" }]
+    };
+    @Watch("switch")
+    private changeSwitch() {
+        this.changeVisiable();
     }
-  }
-  private closeEditUser() {
-    this.$nextTick(() => {
-      const ref: any = this.$refs["editUserForm"];
-      ref.resetFields();
-    });
-    this.switch = !this.switch;
-  }
-  private submitUser() {
-    let params: any = this.editUserForm;
-    (this.$refs["editUserForm"] as any).validate(async (valid: boolean) => {
-      if (valid) {
-        try {
-          if (this.editUserId) {
-            params.user_id = this.editUserId;
-            await api.user_update(params);
-            this.$notify({
-              title: "成功",
-              message: "用户编辑完成",
-              position: "bottom-right",
-              type: "success"
-            });
-            this.closeEditUser();
-            this.sendEmit();
-          } else {
-            await api.user_register(params);
-            this.$notify({
-              title: "成功",
-              message: "用户新增完成",
-              position: "bottom-right",
-              type: "success"
-            });
-            this.closeEditUser();
-            this.sendEmit();
-          }
-          UserModule.getUserInfo();
-        } catch (error) {
-          console.log(error);
-          this.$notify({
-            title: "失败",
-            message: "系统异常，操作失败",
-            position: "bottom-right",
-            type: "error"
-          });
-        }
-      } else {
+    @Emit("update:editUserVisible")
+    private changeVisiable() {
         return false;
-      }
-    });
-  }
-  mounted() {}
+    }
+    @Emit("initList")
+    private sendEmit() {
+        return;
+    }
+    handleAvatarSuccess(res: any, file: any) {
+        if (res.code == 0) {
+            this.editUserForm.avatar = res.result.data;
+        } else {
+            this.editUserForm.avatar = "";
+        }
+    }
+    private closeEditUser() {
+        this.$nextTick(() => {
+            const ref: any = this.$refs["editUserForm"];
+            ref.resetFields();
+        });
+        this.switch = !this.switch;
+    }
+    private submitUser() {
+        let params: any = this.editUserForm;
+        (this.$refs["editUserForm"] as any).validate(async (valid: boolean) => {
+            if (valid) {
+                try {
+                    if (this.editUserId) {
+                        params.user_id = this.editUserId;
+                        await api.user_update(params);
+                        this.$notify({
+                            title: "成功",
+                            message: "用户编辑完成",
+                            position: "bottom-right",
+                            type: "success"
+                        });
+                        this.closeEditUser();
+                        this.sendEmit();
+                    } else {
+                        await api.user_register(params);
+                        this.$notify({
+                            title: "成功",
+                            message: "用户新增完成",
+                            position: "bottom-right",
+                            type: "success"
+                        });
+                        this.closeEditUser();
+                        this.sendEmit();
+                    }
+                    UserModule.getUserInfo();
+                } catch (error) {
+                    console.log(error);
+                    this.$notify({
+                        title: "失败",
+                        message: "系统异常，操作失败",
+                        position: "bottom-right",
+                        type: "error"
+                    });
+                }
+            } else {
+                return false;
+            }
+        });
+    }
+    mounted() {}
 }
 </script>
 
 <style lang='scss'>
 .edituser-dialog {
-  .el-dialog {
-    width: 600px;
-  }
-  .avatar-uploader .el-upload {
-    border: 1px dashed #d9d9d9;
-    border-radius: 6px;
-    cursor: pointer;
-    position: relative;
-    overflow: hidden;
-  }
-  .avatar-uploader .el-upload:hover {
-    border-color: #409eff;
-  }
-  .avatar-uploader-icon {
-    font-size: 28px;
-    color: #8c939d;
-    width: 100px;
-    height: 60px;
-    line-height: 60px;
-    text-align: center;
-  }
-  .avatar {
-    width: 100px;
-    height: 100px;
-    display: block;
-  }
+    .el-dialog {
+        width: 600px;
+    }
+    .avatar-uploader .el-upload {
+        border: 1px dashed #d9d9d9;
+        border-radius: 6px;
+        cursor: pointer;
+        position: relative;
+        overflow: hidden;
+    }
+    .avatar-uploader .el-upload:hover {
+        border-color: #409eff;
+    }
+    .avatar-uploader-icon {
+        font-size: 28px;
+        color: #8c939d;
+        width: 100px;
+        height: 60px;
+        line-height: 60px;
+        text-align: center;
+    }
+    .avatar {
+        width: 100px;
+        height: 100px;
+        display: block;
+    }
 }
 </style>
--- src/views/user/index.vue
+++ src/views/user/index.vue
@@ -1,74 +1,57 @@
 <template>
-  <div class="user-list-container main-content-container">
-    <div class="header-query">
-      <el-form :inline="true" :model="queryForm" class="demo-form-inline" label-width="60px">
-        <el-form-item label="用户名">
-          <el-input size="medium" clearable v-model="queryForm.username" placeholder="名称"></el-input>
-        </el-form-item>
-        <el-form-item label=" ">
-          <el-button size="medium" type="primary" @click="startSearch">查询</el-button>
-          <el-button size="medium" type="success" @click="registerUser">注册</el-button>
-        </el-form-item>
-      </el-form>
+    <div class="user-list-container main-content-container">
+        <div class="header-query">
+            <el-form :inline="true" :model="queryForm" class="demo-form-inline" label-width="60px">
+                <el-form-item label="用户名">
+                    <el-input size="medium" clearable v-model="queryForm.username" placeholder="名称"></el-input>
+                </el-form-item>
+                <el-form-item label=" ">
+                    <el-button size="medium" type="primary" @click="startSearch">查询</el-button>
+                    <el-button size="medium" type="success" @click="registerUser">注册</el-button>
+                </el-form-item>
+            </el-form>
+        </div>
+        <div class="list-wrap">
+            <el-table :data="userList" border stripe style="width: 100%" v-loading="userListLoading"
+                element-loading-text="拼命加载中" element-loading-spinner="el-icon-loading"
+                element-loading-background="rgba(255, 255, 255, 0.5)">
+                <el-table-column align="center" prop="avator" label="头像">
+                    <template slot-scope="scope">
+                        <el-image fit="cover" class="table-img_icon" :src="scope.row.avatar">
+                            <div slot="error" class="image-slot">
+                                <i class="el-icon-picture-outline"></i>
+                            </div>
+                        </el-image>
+                    </template>
+                </el-table-column>
+                <el-table-column align="center" prop="username" label="用户名"></el-table-column>
+                <el-table-column align="center" prop="role_name" label="角色">
+                    <template slot-scope="scope">
+                        <span>{{scope.row.role_name || "未知"}}</span>
+                    </template>
+                </el-table-column>
+                <el-table-column align="center" prop="phone" label="联系方式">
+                    <template slot-scope="scope">
+                        <span>{{scope.row.phone || "---"}}</span>
+                    </template>
+                </el-table-column>
+                <el-table-column min-width="120" align="center" label="操作">
+                    <template slot-scope="scope">
+                        <el-button type="text" @click="editUser(scope.row)">编辑</el-button>
+                        <el-button @click="delUser(scope.row)" type="text" style="color:#f56c6c;">删除</el-button>
+                    </template>
+                </el-table-column>
+            </el-table>
+            <div class="bottom-pagination">
+                <el-pagination @size-change="handleSizeChange" @current-change="handleCurrentChange"
+                    :current-page="pagination.currentPage" :page-sizes="[5, 10, 15, 50]"
+                    :page-size="pagination.pageSize" layout="total, sizes, prev, pager, next, jumper"
+                    :total="pagination.totalSize"></el-pagination>
+            </div>
+            <user-edit :editUserVisible.sync="editUserVisible" :editUserForm="editUserForm"
+                :editUserTitle="editUserTitle" :editUserId="editUserId" @initList="getUserList"></user-edit>
+        </div>
     </div>
-    <div class="list-wrap">
-      <el-table
-        :data="userList"
-        border
-        stripe
-        style="width: 100%"
-        v-loading="userListLoading"
-        element-loading-text="拼命加载中"
-        element-loading-spinner="el-icon-loading"
-        element-loading-background="rgba(255, 255, 255, 0.5)"
-      >
-        <el-table-column align="center" prop="avator" label="头像">
-          <template slot-scope="scope">
-            <el-image fit="cover" class="table-img_icon" :src="scope.row.avatar">
-              <div slot="error" class="image-slot">
-                <i class="el-icon-picture-outline"></i>
-              </div>
-            </el-image>
-          </template>
-        </el-table-column>
-        <el-table-column align="center" prop="username" label="用户名"></el-table-column>
-        <el-table-column align="center" prop="role_name" label="角色">
-          <template slot-scope="scope">
-            <span>{{scope.row.role_name || "未知"}}</span>
-          </template>
-        </el-table-column>
-        <el-table-column align="center" prop="phone" label="联系方式">
-          <template slot-scope="scope">
-            <span>{{scope.row.phone || "---"}}</span>
-          </template>
-        </el-table-column>
-        <el-table-column min-width="120" align="center" label="操作">
-          <template slot-scope="scope">
-            <el-button type="text" @click="editUser(scope.row)">编辑</el-button>
-            <el-button @click="delUser(scope.row)" type="text" style="color:#f56c6c;">删除</el-button>
-          </template>
-        </el-table-column>
-      </el-table>
-      <div class="bottom-pagination">
-        <el-pagination
-          @size-change="handleSizeChange"
-          @current-change="handleCurrentChange"
-          :current-page="pagination.currentPage"
-          :page-sizes="[5, 10, 15, 50]"
-          :page-size="pagination.pageSize"
-          layout="total, sizes, prev, pager, next, jumper"
-          :total="pagination.totalSize"
-        ></el-pagination>
-      </div>
-      <user-edit
-        :editUserVisible.sync="editUserVisible"
-        :editUserForm="editUserForm"
-        :editUserTitle="editUserTitle"
-        :editUserId="editUserId"
-        @initList="getUserList"
-      ></user-edit>
-    </div>
-  </div>
 </template>
 <script lang='ts'>
 // @ is an alias to /src
@@ -76,157 +59,155 @@ import api from "@/api";
 import { Component, Vue, Emit } from "vue-property-decorator";
 import UserEdit from "./components/UserEdit.vue";
 interface QueryJson {
-  username?: string;
+    username?: string;
 }
 interface IeditUserForm {
-  user_id?: string;
-  username: string;
-  password: string;
-  avatar?: string;
-  phone?: string;
-  role?: string;
+    user_id?: string;
+    username: string;
+    password: string;
+    avatar?: string;
+    phone?: string;
+    role?: string;
 }
 @Component({
-  name: "Home",
-  components: {
-    UserEdit
-  }
+    name: "Home",
+    components: {
+        UserEdit
+    }
 })
 export default class Home extends Vue {
-  private userListLoading: boolean = false;
-  private userList: any = [];
-  private queryForm: QueryJson = {};
-  private pagination: {
-    totalSize: number;
-    currentPage: number;
-    pageSize: number;
-  } = {
-    totalSize: 0,
-    currentPage: 1,
-    pageSize: 10
-  };
-  private editUserForm: IeditUserForm = {
-    username: "",
-    password: ""
-  };
-  private editUserVisible: boolean = false;
-  private editUserTitle: string = "注册用户";
-  private editUserId: string = "";
-  @Emit("changeVal")
-  private changeValue() {
-    return 10;
-  }
-  private startSearch() {
-    this.pagination.currentPage = 1;
-    this.getUserList();
-  }
-  private resetForm() {
-    return {
-      user_id: "",
-      username: "",
-      password: "",
-      avatar: "",
-      phone: "",
-      role: ""
+    private userListLoading: boolean = false;
+    private userList: any = [];
+    private queryForm: QueryJson = {};
+    private pagination: {
+        totalSize: number;
+        currentPage: number;
+        pageSize: number;
+    } = {
+        totalSize: 0,
+        currentPage: 1,
+        pageSize: 10
     };
-  }
-  private editUser(row: any) {
-    this.editUserId = row._id;
-    this.editUserVisible = true;
-    this.editUserTitle = "编辑用户";
-    this.editUserForm = JSON.parse(JSON.stringify(row));
-  }
-  private registerUser() {
-    this.editUserId = "";
-    this.editUserVisible = true;
-    this.editUserTitle = "注册用户";
-    this.editUserForm = this.resetForm();
-  }
-  private delUser(row: any) {
-    this.$confirm("此操作将永久删除该用户, 是否继续?", "提示", {
-      confirmButtonText: "确定",
-      cancelButtonText: "取消",
-      type: "warning"
-    })
-      .then(async () => {
-        let params = {
-          user_id: row._id
-        };
-        api
-          .user_del(params)
-          .then(res => {
-            this.getUserList();
-            this.$notify({
-              title: "成功",
-              message: "删除成功",
-              position: "bottom-right",
-              type: "success"
-            });
-          })
-          .catch(err => {
-            console.log(err);
-            this.$notify({
-              title: "成功",
-              message: "删除失败",
-              position: "bottom-right",
-              type: "warning"
-            });
-          });
-      })
-      .catch(err => {
-        console.log(err);
-        this.$message({
-          type: "info",
-          message: "已取消删除"
-        });
-      });
-  }
-
-  private async getUserList() {
-    let params = {
-      pageSize: this.pagination.pageSize,
-      pageNum: this.pagination.currentPage,
-      username: this.queryForm.username
+    private editUserForm: IeditUserForm = {
+        username: "",
+        password: ""
     };
-    this.userListLoading = true;
-    try {
-      let { data } = await api.user_list(params);
-      this.userList = data.list;
-      this.pagination.totalSize = data.total;
-    } catch (error) {
-      console.log(error);
-      this.userList = [];
-      this.pagination.totalSize = 0;
+    private editUserVisible: boolean = false;
+    private editUserTitle: string = "注册用户";
+    private editUserId: string = "";
+    @Emit("changeVal")
+    private changeValue() {
+        return 10;
     }
-    this.userListLoading = false;
-  }
-  handleSizeChange(val: number) {
-    this.pagination.pageSize = val;
-    this.getUserList();
-  }
-  handleCurrentChange(val: number) {
-    this.pagination.currentPage = val;
-    this.getUserList();
-  }
-  mounted() {
-    this.getUserList();
-  }
+    private startSearch() {
+        this.pagination.currentPage = 1;
+        this.getUserList();
+    }
+    private resetForm() {
+        return {
+            user_id: "",
+            username: "",
+            password: "",
+            avatar: "",
+            phone: "",
+            role: ""
+        };
+    }
+    private editUser(row: any) {
+        this.editUserId = row._id;
+        this.editUserVisible = true;
+        this.editUserTitle = "编辑用户";
+        this.editUserForm = JSON.parse(JSON.stringify(row));
+    }
+    private registerUser() {
+        this.editUserId = "";
+        this.editUserVisible = true;
+        this.editUserTitle = "注册用户";
+        this.editUserForm = this.resetForm();
+    }
+    private delUser(row: any) {
+        this.$confirm("此操作将永久删除该用户, 是否继续?", "提示", {
+            confirmButtonText: "确定",
+            cancelButtonText: "取消",
+            type: "warning"
+        })
+            .then(async () => {
+                let params = {
+                    user_id: row._id
+                };
+                api.user_del(params)
+                    .then(res => {
+                        this.getUserList();
+                        this.$notify({
+                            title: "成功",
+                            message: "删除成功",
+                            position: "bottom-right",
+                            type: "success"
+                        });
+                    })
+                    .catch(err => {
+                        console.log(err);
+                        this.$notify({
+                            title: "成功",
+                            message: "删除失败",
+                            position: "bottom-right",
+                            type: "warning"
+                        });
+                    });
+            })
+            .catch(err => {
+                console.log(err);
+                this.$message({
+                    type: "info",
+                    message: "已取消删除"
+                });
+            });
+    }
+
+    private async getUserList() {
+        let params = {
+            pageSize: this.pagination.pageSize,
+            pageNum: this.pagination.currentPage,
+            username: this.queryForm.username
+        };
+        this.userListLoading = true;
+        try {
+            let { data } = await api.user_list(params);
+            this.userList = data.list;
+            this.pagination.totalSize = data.total;
+        } catch (error) {
+            console.log(error);
+            this.userList = [];
+            this.pagination.totalSize = 0;
+        }
+        this.userListLoading = false;
+    }
+    handleSizeChange(val: number) {
+        this.pagination.pageSize = val;
+        this.getUserList();
+    }
+    handleCurrentChange(val: number) {
+        this.pagination.currentPage = val;
+        this.getUserList();
+    }
+    mounted() {
+        this.getUserList();
+    }
 }
 </script>
 <style lang='scss'>
 .user-list-container {
-  .list-wrap {
-    padding: 5px;
-    background: #fff;
-    border-radius: 3px;
-    .table-img_icon {
-      max-width: 100px;
-      max-height: 60px;
-      display: flex;
-      justify-content: space-around;
-      align-items: center;
-      margin: auto;
+    .list-wrap {
+        padding: 5px;
+        background: #fff;
+        border-radius: 3px;
+        .table-img_icon {
+            width: 50px;
+            display: flex;
+            justify-content: space-around;
+            align-items: center;
+            margin: auto;
+        }
     }
-  }
 }
 </style>
